9ba25147c72bd606fb92cb86b104a058c84e7e6b
kino-ma <ma@kino.ma>
2024-05-17 15:57:48 +0900

    chore: initialize repository
    https://github.com/kino-ma/flake-templates#rust

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        name = cargoToml.package.name;
+      in rec
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ cargo rustc rust-analyzer ];
+
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        };
+
+        packages = rec {
+          "${name}" = pkgs.rustPlatform.buildRustPackage
+            {
+              inherit (cargoToml.package) name version;
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+            };
+        };
+        defaultPackage = packages.${name};
+
+        apps = rec {
+          "${name}" = {
+            type = "app";
+            program = "${self.packages.${system}.${name}}/bin/${name}";
+          };
+        };
+        defaultApp = apps.${name};
+      });
+}
