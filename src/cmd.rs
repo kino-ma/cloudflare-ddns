f47e7cfac5f5bb2b3a4a1c96aea5733716727418
kino-ma <ma@kino.ma>
2024-05-22 21:48:42 +0900

    feat: parse all required arguments

--- src/cmd.rs
+++ src/cmd.rs
@@ -3,12 +3,21 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    #[arg(short = 't', long = "token")]
+    pub name: String,
+    pub content: String,
+
+    #[arg(short, long)]
     pub token: String,
 
-    #[arg(short = 'k', long = "key")]
+    #[arg(short, long)]
     pub key: String,
 
-    #[arg(short = 'i', long = "zone-identifier")]
+    #[arg(short = 'i', long)]
     pub zone_identifier: String,
+
+    #[arg(long)]
+    pub ttl: Option<String>,
+
+    #[arg(long)]
+    pub proxied: Option<String>,
 }
