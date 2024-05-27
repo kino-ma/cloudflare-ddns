13606258164b72a61508ae3c486f7460aa0b7346
kino-ma <ma@kino.ma>
2024-05-27 19:01:48 +0900

    feat: get ip

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use std::error::Error;
 
 use clap::Parser;
+
 use cmd::Cli;
+use ip::{get_ipv4, get_ipv6};
 
 mod cmd;
 mod ddns;
 mod ip;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     let configs = cli.get_config()?;
     let params = cli.get_params();
 
     println!("configs = {configs:?}");
     println!("params  = {params:?}");
+
+    let ipv4 = get_ipv4(None).await?;
+    let ipv6 = get_ipv6(None).await?;
+
+    println!("IPv4 = {ipv4}");
+    println!("IPv6 = {ipv6}");
 
     Ok(())
 }
